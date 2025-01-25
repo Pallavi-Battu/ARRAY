2fb1bb56870c8d33f88ebd557acef1936cd4a6a3
Pallavi-Battu <pallavibattu5@gmail.com>
2025-01-25 12:39:23 +0530

    Update arrray.java

--- arrray.java
+++ arrray.java
@@ -1,28 +1,18 @@
 import java.util.Scanner;
 public class arrray {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // Define the size of the array
+        Scanner scanner = new Scanner(System.in); 
         System.out.print("Enter the number of elements in the array: ");
         int size = scanner.nextInt();
-
-        // Create the array
-        int[] array = new int[size];
-
-        // Input elements into the array
+        int[] array = new int[size]; 
         System.out.println("Enter " + size + " elements:");
         for (int i = 0; i < size; i++) {
             array[i] = scanner.nextInt();
         }
-
-        // Print the elements of the array
         System.out.println("The elements of the array are:");
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
         }
-
-        // Close the scanner
         scanner.close();
     }
 }
